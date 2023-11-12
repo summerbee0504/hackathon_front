14d1b3828a7fab592d0e79fd654399967b14947a
yuriafujii <126153199+summerbee0504@users.noreply.github.com>
2023-11-12 11:19:02 +0900

    ページ制限修正

--- my-app/src/authenticaion/AuthContext.tsx
+++ my-app/src/authenticaion/AuthContext.tsx
@@ -5,36 +5,42 @@ import { User } from 'firebase/auth';
 interface IAuthContext {
   currentUser: User | null | undefined;
   isSignedIn: boolean;
+  loading?: boolean;
 }
 
 const AuthContext = createContext<IAuthContext>({
   currentUser: undefined,
-  isSignedIn: false
+  isSignedIn: false,
+  loading: true
 });
 
 const AuthProvider = (props: any) => {
   const [currentUser, setCurrentUser] = useState<User | null | undefined>(undefined);
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    auth.onAuthStateChanged((user) => {
+    return auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
+      setLoading(false);
+      setIsSignedIn(!!user);
     });
   }, []);
 
-  useEffect(() => {
-    if (currentUser) {
-      setIsSignedIn(true);
-    } else {
-      setIsSignedIn(false);
-    }
-  }, [currentUser]);
+  // useEffect(() => {
+  //   if (currentUser) {
+  //     setIsSignedIn(true);
+  //   } else {
+  //     setIsSignedIn(false);
+  //   }
+  // }, [currentUser]);
 
   return (
     <AuthContext.Provider
       value={{
         currentUser: currentUser,
-        isSignedIn: isSignedIn
+        isSignedIn: isSignedIn,
+        loading: loading
       }}
     >
       {props.children}
